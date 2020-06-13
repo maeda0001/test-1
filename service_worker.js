2fef116616882d7d80da73937ddc61e74472621c
pwa-test-904 <66860523+pwa-test-904@users.noreply.github.com>
2020-06-13 16:25:32 +0900

    Add files via upload

---
+++ service_worker.js
@@ -0,0 +1,10 @@
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
