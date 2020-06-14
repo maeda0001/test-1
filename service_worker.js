79764b0ab3f1c117985e897a7e0caccd63ee6523
pwa-test-904 <66860523+pwa-test-904@users.noreply.github.com>
2020-06-14 19:16:42 +0900

    Add files via upload

--- service_worker.js
+++ service_worker.js
@@ -1,3 +1,29 @@
+var urlsToCache = [
+    "index.html",
+    "offline.html"
+];
+
+
+// キャッシュ名 なんでもいい
+var CACHE_NAME  = "pwa-test-cache";
+const CACHE_KEYS = [
+  CACHE_NAME
+];
+
+// インストール時の処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+          .then(
+          function(cache){
+              return cache.addAll(urlsToCache);
+          })
+    );
+});
+
+
+
+
 // リソースフェッチ時のキャッシュロード処理
 self.addEventListener('fetch', function(event) {
     event.respondWith(
